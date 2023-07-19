0561d03027213545062b7b05ce56fa18ad3e06e8
Marcus Blackhall <marcusinamsterdam@gmail.com>
2023-07-19 19:08:04 +0000

    Add routing to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,32 @@ import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Observable, of, tap } from 'rxjs';
+
 import { MakeredDirective } from './makered.directive';
 import { HttpClientModule } from '@angular/common/http';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { ListUsers } from './users/listuser';
+import { PageNotFound } from './pagenotfound';
+
+const routes:Routes = [
+
+  {path: 'users' , component : ListUsers},
+  { path: '',   redirectTo: '/users', pathMatch: 'full' },
+  {path: '**' , component : PageNotFound }
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MakeredDirective
+    MakeredDirective,
+    ListUsers,
+    PageNotFound
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pagenotfound.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+
+
+@Component({
+    selector: 'pagenotfound',
+    templateUrl: './pagenotfound.html'
+    
+  })
+export class PageNotFound {
+}
+
+
